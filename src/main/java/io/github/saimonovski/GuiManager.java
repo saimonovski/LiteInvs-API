31ddfc772130cc0664fc02c333e9b1d979c6f0d3
saimonovski <saimonovski069@gmail.com>
2025-02-23 20:45:55 +0100

    javadocs

--- src/main/java/io/github/saimonovski/GuiManager.java
+++ src/main/java/io/github/saimonovski/GuiManager.java
@@ -19,23 +19,53 @@ public class GuiManager {
 
     /**
      * A method to get instance of manager (singleton)
-     * @return a instance of gui manager 
+     * @return a instance of gui manager
      */
     public static  GuiManager getInstance(){
         return manager != null ? manager : new GuiManager();
     }
+
+    /**
+     * The constructor is private because can exist only one instance of this class
+     */
     private GuiManager(){}
     private final Map<Inventory, InventoryHandler> activeInventories = new HashMap<>();
+
+    /**
+     * A method to register your inventory to a manager, recommended to use is<b> <code>openInventory(Player
+     * player,
+     * InventoryHandler inventoryHandler) <code> </b> because that method will automatically open and register your
+     * inventory
+     * @param inventory wchich should be register
+     * @param handler for it will be signed inventory object
+     */
     public void registerInventory(Inventory inventory, InventoryHandler handler){
         this.activeInventories.put(inventory, handler);
     }
+
+    /**
+     *  If you don't wanna more use your inventory to any of custom holders you can just unregister it here
+     * @param inventory inventory to unregister
+     */
     public void unregisterInventory(Inventory inventory){
         this.activeInventories.remove(inventory);
     }
+
+    /**
+     * This method will automatically open inventory and register it to the activeInventories
+     * @param player player to open inventory
+     * @param inventoryHandler handler to register inventory and getInventory from
+     */
     public void openInventory(Player player, InventoryHandler inventoryHandler){
         this.activeInventories.put(inventoryHandler.getInventory(), inventoryHandler);
         player.openInventory(inventoryHandler.getInventory());
     }
+
+    /**
+     * This method is for handle clicking, will find <b>InventoryHandler</b> and next make on the <b>onClick()</b>
+     * method
+     * @param event event to handle this method
+     */
     public void handleClick(InventoryClickEvent event){
         InventoryHandler handler = activeInventories.get(event.getInventory());
         if(handler == null) return;
